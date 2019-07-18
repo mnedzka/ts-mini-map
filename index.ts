57be9061c178bb2d12eaa759d74ed4a283d9e98f
Michal Nedzka <michael.nedzka@gmail.com>
2019-07-18 21:10:51 +0200

    Connect to google maps

--- index.ts
+++ index.ts
@@ -6,3 +6,11 @@ console.log(user);
 
 const company = new Company();
 console.log(company);
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
