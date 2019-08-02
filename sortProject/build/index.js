fb14aac90939bc28c1290ecba531fa420ba5dc65
Michal Nedzka <michael.nedzka@gmail.com>
2019-08-02 17:11:19 +0200

    Sort strings collections

--- sortProject/build/index.js
+++ sortProject/build/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
-var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
+var numbersCollection = new NumbersCollection_1.NumbersCollection([100, 3, -5, 0]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
--- sortProject/src/index.ts
+++ sortProject/src/index.ts
@@ -1,7 +1,12 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
-console.log(numbersCollection.data);
+// const numbersCollection = new NumbersCollection([100, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const characterCollection = new CharactersCollection('awrgargeaaXfwef');
+const sorter = new Sorter(characterCollection);
+console.log(characterCollection.data);
