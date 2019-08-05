9bed117c5dd4580c6c66fe403eab372589e571c3
Michal Nedzka <michael.nedzka@gmail.com>
2019-08-05 22:15:30 +0200

    Abstract class for Sorter

--- sortProject/src/index.ts
+++ sortProject/src/index.ts
@@ -1,16 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
-import { CharactersCollection } from './CharacterCollection';
+import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([100, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
+// numbersCollection.sort();
 // console.log(numbersCollection.data);
 
-// const characterCollection = new CharacterCollection('awrgargeaaXfwef');
-// const sorter = new Sorter(characterCollection);
-// console.log(characterCollection.data);
+// const charactersCollection = new CharactersCollection('awrgargeaaXfwef');
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -19,6 +18,5 @@ linkedList.add(-3);
 linkedList.add(4);
 linkedList.add(2);
 
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
