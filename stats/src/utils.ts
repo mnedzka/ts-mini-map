6aad844fcf93dd08e544a460a0255d8977bd0685
Michal Nedzka <michael.nedzka@gmail.com>
2019-08-09 20:44:01 +0200

    Add util function for converting dates

---
+++ stats/src/utils.ts
@@ -0,0 +1,5 @@
+export const dateToStringToDate = (dateString: string): Date => {
+  const dateParts = dateString.split('/').map((value: string): number => parseInt(value));
+
+  return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
+};
