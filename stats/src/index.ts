87a33da37b4194cc41a8ad761ef35e74a4586f76
Michal Nedzka <michael.nedzka@gmail.com>
2019-08-10 17:31:57 +0200

    Mapping one row of data

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,15 +1,10 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './MatchResult';
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
 
 let manUnitedWins = 0;
-
-enum MatchResult {
-  homeWin = 'H',
-  awayWin = 'A',
-  draw = 'D',
-}
 
 for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.homeWin) {
