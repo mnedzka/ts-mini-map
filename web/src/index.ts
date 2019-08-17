eea61e879e0ff1e84b40f812b48871b91a426c8b
Michal Nedzka <michael.nedzka@gmail.com>
2019-08-17 17:10:44 +0200

    Add optional keys and set method

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,8 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'John', age: 20 });
+const user = new User({});
+
+user.set({ name: 'Deep', age: 99 });
 
 console.log(user.get('name'));
 console.log(user.get('age'));
