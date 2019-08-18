fcb06e65922f6f271956758776f9193a88936691
Michal Nedzka <michael.nedzka@gmail.com>
2019-08-18 22:04:04 +0200

    Handle trigger change

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,8 +2,14 @@ import { User } from './models/User';
 
 const user = new User({});
 
-user.on('change', () => {});
-user.on('hover', () => {});
-user.on('change', () => {});
+user.on('change', () => {
+  console.log('change no 1');
+});
+user.on('hover', () => {
+  console.log('change no 2');
+});
+user.on('change', () => {
+  console.log('change no 3');
+});
 
-console.log(user);
+user.trigger('change');
