111471fd8bb1a7d999de2d62ba5b5124df455392
Michal Nedzka <michael.nedzka@gmail.com>
2019-08-20 20:53:14 +0200

    Add fetch method in User class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,5 @@
-import axios from 'axios';
+import { User } from './models/User';
 
-axios.post('http://localhost:3000/users', {
-  name: 'name',
-  age: 20,
-});
+const user = new User({ id: 1 });
+
+user.fetch();
