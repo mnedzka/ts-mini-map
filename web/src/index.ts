88cc84425a575b74de64c3d86fdce8e175737082
Michal Nedzka <michael.nedzka@gmail.com>
2019-08-24 14:11:49 +0200

    Start to refactor to Sync class

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'aaa', age: 9999 });
 
-// user.set({ name: 'newName', age: 69 });
-user.save();
+user.events.on('change', () => {
+  console.log('change');
+});
+
+user.events.trigger('change');
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
 interface UserProps {
   id?: number;
@@ -7,6 +7,8 @@ interface UserProps {
 }
 
 export class User {
+  public events: Eventing = new Eventing();
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -15,21 +17,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`).then(
-      (response: AxiosResponse): void => {
-        this.set(response.data);
-      },
-    );
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    }
-    axios.post(`http://localhost:3000/users`, this.data);
   }
 }
